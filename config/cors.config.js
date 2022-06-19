c18c8857b2cd70c1be1a174ec91996f1b62f7ed7
asierbayon <bayonasier@gmail.com>
2022-06-19 18:25:07 +0200

    add juslaunched.xyz to cors

--- config/cors.config.js
+++ config/cors.config.js
@@ -1,7 +1,7 @@
 const createError = require('http-errors');
 const cors = require('cors');
 
-const allowedOrigins = (process.env.CORS_ORIGINS || ['http://localhost:8083', 'https://web-jade-phi.vercel.app'])
+const allowedOrigins = (process.env.CORS_ORIGINS || ['http://localhost:8083', 'https://justlaunched.xyz'])
   .split(',')
   .map(o => o.trim());
 
